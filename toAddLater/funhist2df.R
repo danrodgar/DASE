a7c4aed36115a57a8de43a84031a236327ec8665
JAVIER <jjdolado@gmail.com>
2021-02-09 10:41:22 +0100

    rpackages minor changes

---
+++ toAddLater/funhist2df.R
@@ -0,0 +1,25 @@
+#' Creates some plots and numeric summary
+#'
+#' @param x vector
+#' @return None
+#'
+#' @examples
+#' x <- sample(1:50, 200, replace = TRUE)
+#' funhist2df(x)
+#' 
+#' @export
+funhist2df <- function(x) {
+  # 1 row and 2 columns
+  par(mfrow = c(1,2))
+  # Histogram
+  hist(x,col = rainbow(30))
+  # Box plot
+  boxplot(x, col = 'green')
+  par(mfrow = c(1,1))
+  # Numeric summary
+  df1 <- data.frame(min = min(x),
+             median = median(x),
+             mean = mean(x),
+             max = max(x))
+  return(df1)
+}
