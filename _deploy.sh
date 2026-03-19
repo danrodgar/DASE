f7ea2c0b10da820d5f77f1cafbb4c123287cecb0
Daniel Rodriguez <danrodgar@gmail.com>
2026-03-19 18:42:50 +0100

    chore(main): align branch naming and refresh standard package guidance

--- _deploy.sh
+++ _deploy.sh
@@ -3,7 +3,7 @@
 set -e
 
 [ -z "${GITHUB_PAT}" ] && exit 0
-[ "${TRAVIS_BRANCH}" != "master" ] && exit 0
+[ "${TRAVIS_BRANCH}" != "main" ] && exit 0
 
 git config --global user.email "Daniel Rodriguez"
 git config --global user.name "danrodgar@gmail.com"
